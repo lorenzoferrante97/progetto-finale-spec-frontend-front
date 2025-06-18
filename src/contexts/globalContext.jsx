908c45499cc14d90365d95a2ff118eeccd51e0e5
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-18 09:15:36 +0200

    add: videogames state

--- src/contexts/globalContext.jsx
+++ src/contexts/globalContext.jsx
@@ -1,7 +1,30 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
 const GlobalContext = createContext();
 
 const GlobalProvider = ({ children }) => {
+
+  // --- VIDEOGAMES STATE -----------------------------------------------------------------------
+
+  // const actions = {
+  //   fetchAllGames: () => dispatch({ type: 'fetchAllGames' }),
+  //   fetchGameById: () => dispatch({ type: 'fetchGameById', payload: id }),
+  // };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'fetchAllGames':
+        return ;
+      case 'fetchGameById':
+        return ;
+      default:
+        return state;
+    }
+  }
+
+  const [games, dispatch] = useReducer(reducer, {games: [], activeGame: {}});
+  
+  
+
   const value = {};
 
   return (
