57bc45ada1ecb1fed59ba4345f9296ae87b42700
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-19 08:36:55 +0200

    add: card style

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,14 +1,15 @@
-export default function Badge( { text, type } ) {
+export default function Badge( { text = "Badge", type = "neutral", className = "" } ) {
 
 const typeClasses = {
-  accent: "bg-accent-soft text-accent-text-low"
+  accent: "bg-accent-soft text-accent-text-low",
+  neutral: "bg-neutral-soft text-neutral-text-low"
 }
 
-const roleClass = type === "accent" ? typeClasses.accent : "";
+const roleClass = type === "accent" ? typeClasses.accent : typeClasses.neutral;
   
   return (
     <>
-      <span className={`rounded-md ${roleClass}`}>{text}</span>
+      <span className={`rounded-md px-2 py-1 font-body-s-bold ${roleClass} ${className}`}>{text}</span>
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,27 @@
 import { memo } from "react";
+import Badge from "./Badge";
+import { NavLink } from "react-router-dom";
 
 export default memo(function Card({ game }) {
 
 
-  const {title, category} = game;
+  const {id, title, category} = game;
 
   return (
     <>
-      <div className="col-span-full md:col-span-2 lg:col-span-4 bg-neutral-base-300 p-2 rounded-lg">
+      <NavLink to={`/game/${id}`} className="rounded-lg col-span-full md:col-span-2 lg:col-span-4 bg-neutral-base-300 p-2 transition-base hover:bg-gradient-to-t hover:from-accent-soft/50 hover:to-neutral-base-300">
+        <div className="">
 
-        {/* image */}
+          {/* image */}
 
-        <div className="flex flex-col gap-2">
-        <span>{title}</span>
-        <span>{category}</span>
+          <div className="flex flex-col gap-2">
+          <span className="font-body-base-bold text-neutral-text-high">{title}</span>
+          <Badge text={category} type="accent" className="w-fit"/>
+          </div>
+
         </div>
-
-      </div>
+      </NavLink>
     </>
   );
 });
+
