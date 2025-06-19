3d0143ca1b11a65f2d8387cc70b1598fcbb7d6cb
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-19 09:08:32 +0200

    edit: added coverUrl to getVideogames res

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,19 +2,6 @@ export default function useFetch() {
 
   const apiBaseUrl = import.meta.env.VITE_API_URL;
 
-  const getVideogames = async () => {
-
-    const res = await fetch(`${apiBaseUrl}/videogames`);
-
-    if(!res.ok) {
-      throw new Error('Failed to fetch videogames');
-    }
-
-    const resJson = await res.json();
-
-    return resJson;
-  }
-
   const getVideogame = async (id) => {
     const res = await fetch(`${apiBaseUrl}/videogames/${id}`);
 
@@ -25,6 +12,28 @@ export default function useFetch() {
     const resJson = await res.json();
 
     return resJson;
+  }
+
+  const getVideogames = async () => {
+
+    const res = await fetch(`${apiBaseUrl}/videogames`);
+
+    if(!res.ok) {
+      throw new Error('Failed to fetch videogames');
+    }
+
+    const resJson = await res.json();
+
+    // --- Get full videogames info -----------------------------
+    const allInfo = await Promise.allSettled(resJson.map(item => getVideogame(item.id)));
+
+    // --- CoverUrl added to results ----------------------------
+    const fullRes = resJson.map(item => {
+      return {...item, coverUrl: allInfo[item.id-1].value?.videogame?.coverUrl}
+
+    });
+
+    return fullRes;
   }
 
   return {getVideogames, getVideogame};
