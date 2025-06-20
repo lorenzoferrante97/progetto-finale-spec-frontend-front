fb39289f2dc67e4bf0d4ae1593ded99b649cb651
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-20 16:58:34 +0200

    add: add to favorites icon button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,19 @@
 import { memo } from "react";
 import Badge from "./Badge";
 import { NavLink } from "react-router-dom";
+import { HeartIcon } from "@phosphor-icons/react";
+import { useGlobalContext } from "../contexts/globalContext";
 
 export default memo(function Card({ game, type }) {
 
 
   const {id, title, category, coverUrl} = game;
 
+  const { addToStorage } = useGlobalContext();
+
   return (
     <>
-      <NavLink to={`/videogame/${id}`} className={`rounded-lg bg-neutral-base-300 p-1 transition-base hover:bg-gradient-to-t hover:from-accent-soft/50 hover:to-neutral-base-300 ${type === "favorites" ? "w-full" : "col-span-2"}`}>
+      <NavLink to={`/videogame/${id}`} className={`relative rounded-lg bg-neutral-base-300 p-1 transition-base hover:bg-gradient-to-t hover:from-accent-soft/50 hover:to-neutral-base-300 ${type === "favorites" ? "w-full" : "col-span-2"}`}>
         <div className="flex flex-col gap-2">
 
           {/* image */}
@@ -28,6 +32,14 @@ export default memo(function Card({ game, type }) {
           </div>
 
         </div>
+
+        {
+          type !== "favorites" &&
+            <div className="lg:hover:cursor-pointer transition-base absolute z-30 top-[-8px] right-[-8px] rounded-full bg-accent-solid p-2" onClick={(e) => {e.preventDefault(); e.stopPropagation(); addToStorage("preferiti", game)}}>
+              <HeartIcon size={16} weight="fill" className="text-white" />
+            </div>
+        }
+
       </NavLink>
     </>
   );
