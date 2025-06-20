c6bb4551fb6fe1990a15ee204a7e8e18d3c0f66a
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-20 17:31:03 +0200

    add: compare detail page

---
+++ src/pages/CompareDetail.jsx
@@ -0,0 +1,14 @@
+export default function CompareDetail() {
+  return (
+    <>
+      <div className="row-grid w-full">
+        {/* --- HEADER ---------------------------- */}
+        <div className="cl-span-full">
+          <h1>Confronta due Videogiochi e scopri quello che fa per te</h1>
+
+        </div>
+
+      </div>
+    </>
+  );
+}
