56d3ce631fa6a9787c435fa49044102840363c11
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-20 09:46:34 +0200

    add: useStorage hook

---
+++ src/hooks/useStorage.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function useStorage() {
+
+  const addToStorage = (value) => {
+    if(value) {
+      if(typeof value === 'string') {
+        localStorage.setItem('value', value);
+      } else {
+        localStorage.setItem('value', JSON.stringify(value));
+      }
+    }
+  }
+
+  const removeFromStorage = (value) => value && localStorage.removeItem('value');
+ 
+
+  const getFromStorage = (value, type = 'string') => {
+   if(value) {
+    switch(type) {
+      case 'string':
+        return localStorage.getItem(value);
+      default:
+        return JSON.parse(localStorage.getItem(value));
+    }
+   }
+  }
+}
