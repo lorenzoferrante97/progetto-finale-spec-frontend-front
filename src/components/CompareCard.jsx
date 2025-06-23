57bc65ce1a1d1485ab18949ef98b31fca51ba4af
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-23 15:39:17 +0200

    edit: compareCard styles

--- src/components/CompareCard.jsx
+++ src/components/CompareCard.jsx
@@ -11,41 +11,53 @@ export default function CompareCard({ game }) {
           <img src={game?.coverUrl} alt={game?.title} />
         </figure>
         {/* --- RATING ---------------- */}
-        <div className="h-20 flex flex-col items-center border-b border-neutral-border/40 px-2 gap-1">
-        <span className="font-body-s-regular text-neutral-text-low">Valutato dai giocatori</span>
-          <span className="font-body-s-bold">{game?.rating}<span className="font-body-s-light"> / 10</span></span>
+        <div className="h-44 perfect-center flex-col border-b border-neutral-border/40 px-2 gap-3">
+          <span className="font-body-s-regular text-neutral-text-low">Valutato dai giocatori</span>
+          <span className="font-h1 text-accent-text-low">{game?.rating}<span className="font-body-base-regular text-accent-text-high"> / 10</span></span>
         </div>
         {/* --- TITLE ---------------- */}
-        <div className="h-20 flex items-center border-b border-neutral-border/40 px-2 ">
+        <div className="h-44 perfect-center flex items-center border-b border-neutral-border/40 px-2 ">
           <h2 className="font-h6 font-semibold">{game?.title}</h2>
         </div>
         {/* --- CATEGORY ---------------- */}
-        <div className="h-20 flex items-center border-b border-neutral-border/40 px-2">
+        <div className="h-44 flex items-center border-b border-neutral-border/40 px-2">
           <Badge text={game?.category} type="accent" />
         </div>
         {/* --- PLATFORM ---------------- */}
-        <div className="h-20 flex items-center border-b border-neutral-border/40 px-2">
-          <Badge text={game?.platform} type="accent" />
-        </div>
-        {/* --- DESCRIPTION ---------------- */}
-        <div className="h-20 flex items-center border-b border-neutral-border/40 px-2">
-          <p>{game?.description}</p>
+        <div className="h-44 flex flex-col gap-1 items-start justify-center border-b border-neutral-border/40 px-2">
+          {
+            game?.platform?.map((platform) => (
+              <Badge key={platform} text={platform} type="accent" />
+            ))
+          }
         </div>
         {/* --- THEME ---------------- */}
-        <div className="h-20 flex items-center border-b border-neutral-border/40 px-2">
-          <Badge text={game?.theme} type="accent" />
+        <div className="h-44 flex flex-col gap-1 items-start justify-center border-b border-neutral-border/40 px-2">
+          {
+            game?.theme?.map((theme) => (
+              <Badge key={theme} text={theme} type="accent" />
+            ))
+          }
         </div>
         {/* --- DEVELOPER ---------------- */}
-        <div className="h-20 flex items-center border-b border-neutral-border/40 px-2">
+        <div className="h-44 flex items-center border-b border-neutral-border/40 px-2">
           <Badge text={game?.developer} type="accent" />
         </div>
         {/* --- GAME MODE ---------------- */}
-        <div className="h-20 flex items-center border-b border-neutral-border/40 px-2">
-          <Badge text={game?.gameMode} type="accent" />
+        <div className="h-44 flex flex-col gap-1 items-start justify-center border-b border-neutral-border/40 px-2">
+          {
+            game?.gameMode?.map((gameMode) => (
+              <Badge key={gameMode} text={gameMode} type="accent" />
+            ))
+          }
         </div>
         {/* --- RELEASE YEAR ---------------- */}
-        <div className="h-20 flex items-center border-b border-neutral-border/40 px-2">
+        <div className="h-44 flex items-center border-b border-neutral-border/40 px-2">
           <Badge text={game?.releaseYear} type="accent" />
+        </div>
+        {/* --- DESCRIPTION ---------------- */}
+        <div className="min-h-44 flex items-center border-b border-neutral-border/40 px-2">
+          <p className="line-clamp-6">{game?.description}</p>
         </div>
       </div>
     </>
--- src/pages/CompareDetail.jsx
+++ src/pages/CompareDetail.jsx
@@ -28,9 +28,9 @@ const { games, dispatch, getVideogames } = useGlobalContext();
         </div>
 
         {/* --- MAIN -------------------------------------------------------------- */}
-        <div className="col-span-full row-grid bg-neutral-base-200 rounded-lg p-2 lg:p-4 min-h-[60vh] gap-2">
+        <div className={`col-span-full row-grid bg-neutral-base-200 rounded-lg p-2 lg:p-4 gap-2 ${games?.comparedGames?.length === 0 ? 'h-fit' : 'min-h-[60vh]'}`}>
           {/* --- FIRST SELECTOR ---------------- */}
-          <div className="col-span-2 perfect-center md:col-span-4 lg:col-span-6 rounded-lg">
+          <div className="col-span-2 flex flex-col md:col-span-4 lg:col-span-6 rounded-lg">
             {
               games?.comparedGames?.length === 0 ? <Selector games={games?.allGames} /> : <CompareCard game={games?.comparedGames[0]} /> 
             }
@@ -38,7 +38,7 @@ const { games, dispatch, getVideogames } = useGlobalContext();
           </div>
 
           {/* --- SECOND SELECTOR ---------------- */}
-          <div className="col-span-2 perfect-center md:col-span-4 lg:col-span-6 rounded-lg">
+          <div className="col-span-2 flex flex-col md:col-span-4 lg:col-span-6 rounded-lg">
           {
               (games?.comparedGames?.length === 0) || (games?.comparedGames?.length === 1) ? <Selector games={games?.allGames} /> : <CompareCard game={games?.comparedGames[1]} /> 
           }
