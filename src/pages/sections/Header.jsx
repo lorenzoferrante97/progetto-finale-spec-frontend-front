b9ab210c788ad76df86815a93600732d74d8a5f4
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-24 10:44:53 +0200

    add: favorites counter feedback

--- src/pages/sections/Header.jsx
+++ src/pages/sections/Header.jsx
@@ -2,12 +2,18 @@ import { NavLink } from "react-router-dom";
 import { HeartIcon } from "@phosphor-icons/react";
 import Portal from "../../components/Portal";
 import Favorites from "./Favorites";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useGlobalContext } from "../../contexts/globalContext";
 
 export default function Header() {
 
 const [showFavorites, setShowFavorites] = useState(false);
+const { favorites, getFromStorage } = useGlobalContext();
 
+useEffect(() => {
+  getFromStorage("preferiti");
+// eslint-disable-next-line react-hooks/exhaustive-deps
+}, []);
 
   return (
     <>
@@ -18,7 +24,17 @@ const [showFavorites, setShowFavorites] = useState(false);
               <li className="px-2 py-1 rounded-md min-h-12 perfect-center"><NavLink to="/">Home</NavLink></li>
               <li className="px-2 py-1 rounded-md min-h-12 perfect-center"><NavLink to="/compare">Confronta</NavLink></li>
             </div>
-            <li><button onClick={() => setShowFavorites(!showFavorites)} className="lg:hover:bg-accent-soft rounded-full perfect-center p-2 aspect-square min-h-12"><HeartIcon size={20} weight="fill" className="text-accent-text-low" /></button></li>
+            <li className="relative">
+              <button onClick={() => setShowFavorites(!showFavorites)} className="lg:hover:bg-accent-soft rounded-full perfect-center p-2 aspect-square min-h-12">
+                <HeartIcon size={28} weight="fill" className="text-accent-text-low" />
+              </button>
+              {
+                favorites?.length > 0 ?
+                <span className="absolute top-0 right-[-6px] aspect-square size-6 rounded-full font-body-s-bold perfect-center bg-accent-solid/40 backdrop-blur-sm text-white p-1">{favorites?.length}</span>
+                :
+                null
+              }
+            </li>
           </ul>
         </nav>
       </div>
